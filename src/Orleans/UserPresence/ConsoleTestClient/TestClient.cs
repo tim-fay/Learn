bc638e3bda0f146299f43cb91182a2bafc68e65d
Timur Fayzrakhmanov <timur.f@outlook.com>
2017-07-13 15:46:08 +0300

    Added TestClient

---
+++ src/Orleans/UserPresence/ConsoleTestClient/TestClient.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ConsoleTestClient
+{
+    public class TestClient
+    {
+        private HttpClient HttpClient { get; }
+        private long ClientId { get; }
+
+        public TestClient(long clientId)
+        {
+            ClientId = clientId;
+            HttpClient = new HttpClient();
+            //httpClient.BaseAddress = 
+        }
+
+        public Task Run()
+        {
+            //http://localhost:54739/api/values
+            //HttpClient.GetAsync("")
+
+            return Task.CompletedTask;
+        }
+    }
+}
