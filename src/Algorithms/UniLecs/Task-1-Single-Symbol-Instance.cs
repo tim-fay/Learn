413c86134cc196f5c5e70cba492b1c1d97248cc1
Timur Fayzrakhmanov <timur.f@outlook.com>
2017-10-21 17:14:50 +0300

    Task 1 from UniLecs

---
+++ src/Algorithms/UniLecs/Task-1-Single-Symbol-Instance.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace UniLecs
+{
+    public class Task1SingleInstanceInString
+    {
+        [Theory]
+        [InlineData("abcdefg ")]
+        [InlineData(" ")]
+        public void NoDuplicateItem(string input)
+        {
+            Assert.True(CheckIfUniqueItemsOnly(input));
+        }
+
+        [Theory]
+        [InlineData("abcafg")]
+        [InlineData(" Try   ")]
+        [InlineData("")]
+        public void NoUniqueItems(string input)
+        {
+            Assert.False(CheckIfUniqueItemsOnly(input));
+        }
+
+        private bool CheckIfUniqueItemsOnly(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return false;
+            }
+
+            var hashSet = new HashSet<char>();
+
+            foreach (var symbol in input)
+            {
+                if (hashSet.Contains(symbol))
+                {
+                    return false;
+                }
+
+                hashSet.Add(symbol);
+            }
+
+            return true;
+        }
+    }
+}
