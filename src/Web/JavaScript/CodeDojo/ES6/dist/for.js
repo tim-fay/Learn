6625cc793dd7e7d9a725d93d48ad5ccbd39ddecb
Timur Fayzrakhmanov <timur.f@outlook.com>
2017-10-28 13:40:42 +0300

    CodeDojo: for of

---
+++ src/Web/JavaScript/CodeDojo/ES6/dist/for.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var browsers = ['Chrome', 'Firefox', 'Edge', 'Safari', 'Opera'];
+
+for (var browser in browsers) {
+    console.log(browser);
+}
+
+for (var index in browsers) {
+    console.log(browsers[index]);
+}
+
+var _iteratorNormalCompletion = true;
+var _didIteratorError = false;
+var _iteratorError = undefined;
+
+try {
+    for (var _iterator = browsers[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+        var _browser = _step.value;
+
+        console.log(_browser);
+    }
+} catch (err) {
+    _didIteratorError = true;
+    _iteratorError = err;
+} finally {
+    try {
+        if (!_iteratorNormalCompletion && _iterator.return) {
+            _iterator.return();
+        }
+    } finally {
+        if (_didIteratorError) {
+            throw _iteratorError;
+        }
+    }
+}
+
+console.log(browsers);
